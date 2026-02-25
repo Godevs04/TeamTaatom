894656dd0ee662091fbfb6199fa304c2f1cc6c7b
Kavin1421 <kkavinkumar24@gmail.com>
2026-02-25 22:35:16 +0530

    TAATOM-03 Shorst Add

---
+++ frontend/constants/admob.d.ts
@@ -0,0 +1,5 @@
+export const ADMOB: {
+  publisherId: string;
+  android: { banner: string; interstitial: string; native: string };
+  ios: { banner: string; interstitial: string; native: string };
+};
